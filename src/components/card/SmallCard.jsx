acc7757ac777dfc50c5ad5c4b55f6f3c528161a7
LeulBerhanu <leulberhanua@gmail.com>
2023-09-29 20:40:43 +0300

    created small card, and created Blog section

--- src/components/card/SmallCard.jsx
+++ src/components/card/SmallCard.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import house from "../../img/house.png";
 
-function Card() {
+function SmallCard() {
   return (
-    <div className="flex gap-9 items-center px-9 py-8 shadow-boxShadow w-[994px] rounded-2xl ">
-      <img src={house} className="max-w-430px" alt="house" />
+    <div className="flex gap-9 items-center px-5 py-4 shadow-boxShadow w-[610px] rounded-2xl">
+      <img src={house} className="max-w-[278px]" alt="house" />
       {/* Card Body */}
       <div className="flex flex-col gap-y-16">
         {/* Title and Price */}
         <div>
           {/* Title */}
-          <h3 className="capitalize break-words text-5xl font-light">
+          <h3 className="capitalize break-words text-2xl">
             Bole bulbula site, two bedroom
           </h3>
 
           {/* Price */}
-          <p className="text-xl">$97,000/sqm</p>
+          <p className="text-base font-medium">$97,000/sqm</p>
         </div>
 
         {/* Details */}
-        <ul className="flex flex-col text-3xl gap-y-4">
+        <ul className="flex flex-col text-xl gap-y-3 ">
           <li>125sqm</li>
           <li>2 Bedrooms</li>
           <li>2 Bathrooms</li>
@@ -29,4 +29,4 @@ function Card() {
   );
 }
 
-export default Card;
+export default SmallCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import Card from "../components/card/Card";
 import Hero from "../components/Hero";
+import SmallCard from "../components/card/SmallCard";
+// Icons
+import { HiOutlineArrowUpRight } from "react-icons/hi2";
 
-const h2Style = "text-[39px] text-center m-8";
+const h2Style = "text-[39px] text-center m-8 capitalize";
 
 function HomePage() {
   return (
@@ -10,11 +13,53 @@ function HomePage() {
       <h1 className="text-[80px] text-center capitalize">
         Opening doors, <br /> building communities!
       </h1>
-      <section id="slider">
+      <section id="slider_section">
         <Card />
       </section>
-      <h2 className={h2Style}>Why chose shoa Homes?</h2>
+
+      <h2 className={h2Style}>Why choose shoa Homes?</h2>
       <Hero />
+
+      <section id="featured_properties_section">
+        <h2 className={h2Style}>Featured properties</h2>
+        <div className=" flex gap-x-11 justify-center">
+          <SmallCard />
+          <SmallCard />
+        </div>
+      </section>
+
+      <section id="blog_section">
+        <h2 className={h2Style}>Blog</h2>
+        <div className="flex text-4xl justify-center">
+          <div className="flex flex-col gap-16 p-11 max-w-[480px] border">
+            <a href="#">Housing prices in Addis Ababa is changing</a>
+            <i className="self-end ">
+              <HiOutlineArrowUpRight />
+            </i>
+          </div>
+
+          <div className="flex flex-col gap-16 p-11 max-w-[480px] border">
+            <a href="#">
+              Is the current inflation affecting real estate owners
+            </a>
+            <i className="self-end ">
+              <HiOutlineArrowUpRight />
+            </i>
+          </div>
+
+          <div className="flex flex-col gap-16 p-11 max-w-[480px] border">
+            <a href="#">What’s new at shoa real estate listings</a>
+            <i className="w-8 self-end align-baseline">
+              <HiOutlineArrowUpRight />
+            </i>
+          </div>
+        </div>
+      </section>
+
+      <section id="location_section">
+        <h2 className={h2Style}>Location</h2>
+        {/* <LocationMap /> */}
+      </section>
     </>
   );
 }
