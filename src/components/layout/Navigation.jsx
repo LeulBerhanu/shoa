45b885e71dc8c9c29e41960bc82f3464ce2a6f26
LeulBerhanu <leulberhanua@gmail.com>
2023-10-07 09:33:59 +0300

    removed the color change feature in navbar

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -7,30 +7,23 @@ import "./navStyles.css";
 function Navigation() {
   const location = useLocation();
 
-  const [bgChange, setBgChange] = React.useState(false);
+  // const [bgChange, setBgChange] = React.useState(false);
 
-  React.useEffect(() => {
-    if (location.pathname === "/projects") {
-      setBgChange(true);
-    } else {
-      setBgChange(false);
-    }
-  }, [location]);
+  // React.useEffect(() => {
+  //   if (location.pathname === "/projects") {
+  //     setBgChange(true);
+  //   } else {
+  //     setBgChange(false);
+  //   }
+  // }, [location]);
 
-  const style = {
-    color: "white",
-  };
+  // const style = {
+  //   color: "white",
+  // };
 
   return (
-    <div
-      style={bgChange ? style : null}
-      className="flex container mx-auto  align-middle justify-between px-16 py-14 xl:flex"
-    >
-      <img
-        src={bgChange ? shoaLogoWhite : shoaLogo}
-        alt="Shoa logo"
-        className="w-[50px] xl:w-[95px]"
-      />
+    <div className="flex container mx-auto  align-middle justify-between px-16 py-14 xl:flex">
+      <img src={shoaLogo} alt="Shoa logo" className="w-[50px] xl:w-[95px]" />
       <div className="flex space-x-5 font-medium text-sm xl:text-2xl xl:space-x-14 items-center relative">
         <NavLink className="relative" to="/">
           Home
@@ -52,7 +45,7 @@ function Navigation() {
           className={
             location.pathname.includes("/projects/")
               ? "active relative"
-              : ({ isActive }) => (isActive ? "active-white relative" : "")
+              : ({ isActive }) => (isActive ? "active relative" : "")
           }
           to="projects"
         >
