95c4f3bc1dfd92e39ffd8dd889659a180039b4ff
LeulBerhanu <leulberhanua@gmail.com>
2023-10-23 08:39:50 +0300

    properties list

--- src/Admin/Pages/PropertyPage.jsx
+++ src/Admin/Pages/PropertyPage.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { MdSwapVert } from "react-icons/md";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const tableBtn =
   "w-[60px] h-[40px]  flex items-center justify-center  px-4 py-3 font-medium border-2 border-black/25 rounded-lg";
 
 function PropertyPage() {
+  const [properties, setProperties] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/property")
+      .then((res) => res.data.properties)
+      .then((data) => setProperties(data));
+  }, []);
+
   return (
     <div>
       <section className="px-10">
@@ -41,6 +51,31 @@ function PropertyPage() {
               </tr>
             </thead>
             <tbody className="[&>:nth-child(odd)]:bg-black/5 [&>*]:h-20">
+              {properties &&
+                properties.map((property, idx) => (
+                  <tr key={property._id} className="[&>*]:px-10">
+                    <td>{idx + 1}</td>
+                    <td>{property.name}</td>
+                    <td>28 Oct 2023</td>
+                    <td>
+                      <button
+                        className={`${tableBtn} border-2 border-[#A3CFBB] bg-[#D1E7DD]`}
+                      >
+                        Yes
+                      </button>
+                    </td>
+                    <td>
+                      <div className="flex gap-x-3">
+                        <button className={`${tableBtn} bg-white`}>
+                          <Link to={`edit-site/${property._id}`}>Edit</Link>
+                        </button>
+                        <button className={`${tableBtn} bg-white`}>
+                          <RiDeleteBin6Line />
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
               <tr className="[&>*]:px-10">
                 <td>1</td>
                 <td>Bole Bulbula Site, Two bedroom</td>
