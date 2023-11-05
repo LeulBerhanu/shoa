e652665c0bc2277957249a07c46917af6575bef3
LeulBerhanu <leulberhanua@gmail.com>
2023-11-05 22:45:47 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-// Components
+// Component
 import Layout from "./components/layout/Layout";
 // Pages
 import HomePage from "./pages/HomePage";
